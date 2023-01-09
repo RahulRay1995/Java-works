681082492883863973f2e0b94be6688c64de1565
RahulRay1995 <rahulray171995@gmail.com>
2023-01-09 20:51:20 +0000

    Manual3

---
+++ Manualpdf3.java
@@ -0,0 +1,16 @@
+package manualtopic;
+
+public class Manualpdf3 {
+    public static void main(String[] args) {
+        System.out.println("1. What is Bug");
+        System.out.println("2. Bug Lifecycle");
+        System.out.println("3. Why are there Bugs in Software");
+        System.out.println("4. Bug Template");
+        System.out.println("5. Severity Vs Priority");
+        System.out.println("6. What is Severity?");
+        System.out.println("7. What is Priority?");
+        System.out.println("8. Black box testing & White box testing");
+        System.out.println("9. Testing Levels");
+        System.out.println("10. Alpha Testing & Beta Testing");
+    }
+}
