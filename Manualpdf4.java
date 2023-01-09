90739d103f4937b311a047c275473aa9281cbb71
RahulRay1995 <rahulray171995@gmail.com>
2023-01-09 21:04:25 +0000

    Manual4

---
+++ Manualpdf4.java
@@ -0,0 +1,19 @@
+package manualtopic;
+
+import java.sql.SQLOutput;
+
+public class Manualpdf4 {
+    public static void main(String[] args) {
+        System.out.println("1. Static Vs Dynamic Testing");
+        System.out.println("2. What is Exploratory Testing?");
+        System.out.println("3. What is Adhoc Testing?");
+        System.out.println("4. Software Development Models");
+        System.out.println("5. Agile Model");
+        System.out.println("6. Scrum Terminology");
+        System.out.println("7. Fibonacci Series ");
+        System.out.println("8. What is Definition of Done?");
+        System.out.println("9. DoR Vs DoD");
+        System.out.println("10. Scrum Board");
+
+    }
+}
