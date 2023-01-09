615a1ab6c3f2819ebfc9d14d5f9f5dd303661b09
RahulRay1995 <rahulray171995@gmail.com>
2023-01-09 20:28:37 +0000

    Manual2

---
+++ Manualpdf2.java
@@ -0,0 +1,16 @@
+package manualtopic;
+
+public class Manualpdf2 {
+    public static void main(String[] args) {
+        System.out.println("1. Challenges with manual testing ");
+        System.out.println("2. 7 Principles of software testing ");
+        System.out.println("3. Test plan");
+        System.out.println("4. Why is it important to have test plan in software testing?");
+        System.out.println("5. Test plan Template");
+        System.out.println("6. Test Scenario");
+        System.out.println("7. Test Scenario Template");
+        System.out.println("8. Test Case");
+        System.out.println("9. Test case Template");
+        System.out.println("10. requirement traceability matrix");
+    }
+}
